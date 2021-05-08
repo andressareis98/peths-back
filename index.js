ef5dc9f5d7c397c6f9e7336b047d4c2d88adb7fc
Andressa Reis Araujo <andressareis98@outlook.com>
2021-05-08 20:02:27 -0300

    feat: config heroku

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ consign()
 app.db = db;
 
 // Porta em que o projeto será executado
+const url =
+  "postgres://mylgaqbxerbtjh:592e396b57169cd976b3c6d793b459ad4c107416b2b23dccb56587569ad0cc49@ec2-52-0-114-209.compute-1.amazonaws.com:5432/d7l67oqg4rt57q";
 app.listen(3000, () => {
   console.log("Backend executando...");
 });
--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,12 @@
 module.exports = {
   client: "postgresql",
   connection: {
-    database: "peths",
+    /* database: "peths",
     user: "postgres",
-    password: "teste123",
+    password: "teste123", */
+    connectionString:
+      "postgres://mylgaqbxerbtjh:592e396b57169cd976b3c6d793b459ad4c107416b2b23dccb56587569ad0cc49@ec2-52-0-114-209.compute-1.amazonaws.com:5432/d7l67oqg4rt57q",
+    ssl: { rejectUnauthorized: false },
   },
   pool: {
     min: 2,
