54d3de952f1d1f7642018fcc23d58f35b68560fe
Andressa Reis Araujo <andressareis98@outlook.com>
2021-05-09 00:22:48 -0300

    feat: edit routes.js, add get users

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   app.post("/signup", app.api.user.save);
   app.post("/signin", app.api.auth.signin);
 
-  app.get("/users", app.user.getListUsers);
+  app.get("/users", app.api.user.getListUsers);
 
   app
     .route("/pets")
